67e29cdbd51f68a24d50d679fce2ffd028f15678
MasFana <adrianfathan69@gmail.com>
2026-05-21 04:43:38 +0700

    Comment out default root route

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get("/", "Home::index");
+// $routes->get("/", "Home::index");
 $routes->get("api/docs/spec", "DocsController::spec");
 $routes->get("api/docs/specs", "DocsController::spec");
 $routes->get("api/docs", "DocsController::index");
